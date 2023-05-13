7275428614766b3dd3393576a60cd1cd8c1e0955
777 <goszakupki.aktiv@gmail.com>
2023-05-13 19:09:07 +0300

    Начинаем работать с Хайбернейтом с Матвиенко

---
+++ src/Java/JDBC/JdbcRunner.java
@@ -0,0 +1,22 @@
+package Java.JDBC;
+
+import Java.JDBC.Util.ConnectionManager;
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcRunner {
+
+    public static void main(String[] args) throws SQLException {
+
+        Class<Driver> driverClass = Driver.class;
+
+        try(Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
+
+
+    }
+}
