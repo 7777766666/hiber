4afd632b6e868146b21c58c76321cbf15a725874
777 <goszakupki.aktiv@gmail.com>
2023-05-14 02:27:12 +0300

    Добавлен PreparedStatement

--- src/Java/JDBC/JdbcRunner.java
+++ src/Java/JDBC/JdbcRunner.java
@@ -5,6 +5,7 @@ import org.postgresql.Driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,15 +17,38 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
 
         String superLol = "'ccc' or '' = ''";
-        System.out.println(getSuper(superLol));
-        System.out.println(getSuperId(superLol));
+//        System.out.println(getSuper(superLol));
+//        System.out.println(getSuperId(superLol));
+        Long x = 15L;
+        Long z = 196L;
+//        System.out.println(getPrep(x));
+        System.out.println(getBetween(x, z));
+
 
     }
+
+    private static List<String> getPrep(Long x) throws SQLException {
+        List<String> stringList = new ArrayList<>();
+        String sql = """
+                select color from base.it.super_lol where id = ? ;
+                """;
+
+        try (Connection connection = ConnectionManager.open()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, x);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                stringList.add(resultSet.getString("color"));
+            }
+        }
+        return stringList;
+    }
+
 
     private static List<String> getSuper(String x) throws SQLException {
 
         String sql = """
-                
+                                
                 select color from base.it.super_lol where color = %s;
 
                 """.formatted(x);
@@ -38,10 +62,33 @@ public class JdbcRunner {
                 listSuper.add(resultSet.getString("color"));
 //                integerList.add(resultSet.getInt("id"));
             }
-
-
         }
         return listSuper;
+    }
+
+    private static List<String> getBetween(Long l1, Long l2) throws SQLException {
+        List<String> stringList = new ArrayList<>();
+
+        String str = """
+                select color from base.it.super_lol where id BETWEEN ? and ?;
+                """;
+
+        try (Connection connection = ConnectionManager.open()) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(str);
+            System.out.println(preparedStatement);
+            preparedStatement.setLong(1, l1);
+            System.out.println(preparedStatement);
+            preparedStatement.setLong(2, l2);
+            System.out.println(preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                stringList.add(resultSet.getString("color"));
+            }
+
+        }
+        return stringList;
+
     }
 
     private static List<Integer> getSuperId(String x) throws SQLException {
@@ -60,7 +107,6 @@ public class JdbcRunner {
 //                listSuper.add(resultSet.getString("color"));
                 integerList.add(resultSet.getInt("id"));
             }
-
 
         }
         return integerList;
